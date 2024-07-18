4e8e722affd5a27502c0c1bc45cb5d4844c88475
Muhammad Rafid Amrullah <rafid.tijarah@gmail.com>
2024-07-18 20:38:30 +0700

    Update scraper & add wrangler v0

--- data_collector/scraper.py
+++ data_collector/scraper.py
@@ -1,29 +1,40 @@
 import asyncio
+from random import random
 from bs4 import BeautifulSoup
 import httpx
 
 from data_collector.utils import to_df
+from data_collector.user_agents import user_agents
 
 
 async def scrape():
     timeout = httpx.Timeout(5, read=10)
-    params = dict()
-    async with httpx.AsyncClient(timeout=timeout) as client:
-        num_pages = 1171
-        for i in range(num_pages):
-            try:
-                if i % 3 == 0:
-                    print(f"{(i//3)+1}-th interval")
-                    await asyncio.sleep(3)
-                params['page'] = i
-                r = await client.get("https://www.rumah123.com/jual/bandung/tanah/", params=params)
-                soup = BeautifulSoup(r.text, "html.parser")
-                divs = soup.find_all('div', class_='card-featured__middle-section')
-                df = to_df(divs)
-                df.to_csv('csv/house_price_raw.csv', mode='a', index=False, header=False)
-            except:
-                print(f"Failed at index: {i}")
-                break
+    # client = httpx.AsyncClient(timeout=timeout)
+    # num_pages = 1
+    num_pages = 1171
+    i = 1
+    while i <= num_pages:
+        try:
+            # if i % 3 == 0:
+            print(f"Page {i}")
+            await asyncio.sleep(5)
+            params = {'page': i}
+            headers = {'user-agent': user_agents[int(random()*100)]}
+            client = httpx.AsyncClient(timeout=timeout, headers=headers)
+            r = await client.get("https://www.rumah123.com/jual/bandung/tanah/", params=params)
+            await client.aclose()
+            soup = BeautifulSoup(r.text, "html.parser")
+            divs = soup.find_all('div', class_='card-featured__middle-section')
+            if not divs:
+                print(f"Delaying at page: {i}")
+                await asyncio.sleep(300)
+                continue
+            df = to_df(divs)
+            df.to_csv('csv/house_price_raw.csv', mode='a', index=False, header=False)
+            i += 1
+        except:
+            print(f"Failed at page: {i}")
+            continue
 
 
 if __name__ == "__main__":
